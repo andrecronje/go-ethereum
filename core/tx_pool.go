6fb900bb80dbf5b55fded72a9ad5b921246bc00a
Andre Cronje <andre@mfin.io>
2018-03-19 18:29:51 +0200

    No need to journal
    Signed-off-by: Andre Cronje <andre@mfin.io>

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -665,7 +665,7 @@ func (pool *TxPool) add(tx *types.Transaction, local bool) (bool, error) {
 	if local {
 		pool.locals.add(from)
 	}
-	pool.journalTx(from, tx)
+	//pool.journalTx(from, tx)
 
 	log.Trace("Pooled new future transaction", "hash", hash, "from", from, "to", tx.To())
 	return false, nil
