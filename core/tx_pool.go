673e63eecf604c7dcf09e6946c059d2d8262e90c
Andre Cronje <andre@mfin.io>
2018-03-19 18:07:23 +0200

    txpool from for locals
    Signed-off-by: Andre Cronje <andre@mfin.io>

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -634,7 +634,7 @@ func (pool *TxPool) add(tx *types.Transaction, local bool) (bool, error) {
 		}
 	}*/
 	// If the transaction is replacing an already pending one, do directly
-	//from, _ := types.Sender(pool.signer, tx) // already validated
+	from, _ := types.Sender(pool.signer, tx) // already validated
 	/*if list := pool.pending[from]; list != nil && list.Overlaps(tx) {
 		// Nonce already pending, check if required price bump is met
 		inserted, old := list.Add(tx, pool.config.PriceBump)
@@ -670,7 +670,7 @@ func (pool *TxPool) add(tx *types.Transaction, local bool) (bool, error) {
 	}
 	pool.journalTx(from, tx)
 
-	log.Trace("Pooled new future transaction", "hash", hash, "from", "", "to", tx.To())
+	log.Trace("Pooled new future transaction", "hash", hash, "from", from, "to", tx.To())
 	return replace, nil
 }
 
